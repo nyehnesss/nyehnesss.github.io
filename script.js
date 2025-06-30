01daa6b6a0d47609732a941552728dd3fd5c3f66
nyehnesss <lunetta35@gmail.com>
2025-06-30 23:43:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const buttonsContainer = document.getElementById('buttons-container');
 const imageButtons = document.querySelectorAll('.img-button');
 const overlay = document.getElementById('start-overlay');
 
-const interactionPoints = [21, 42, 58, 78, 102, 146]; // Timestamps
+const interactionPoints = [21, 41, 58, 78, 102, 146]; // Timestamps
 let currentInteractionIndex = 0;
 let interactionShown = false;
 
